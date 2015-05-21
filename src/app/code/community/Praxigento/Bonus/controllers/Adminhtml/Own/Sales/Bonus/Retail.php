9916f5034a8396b07662f9b24583c573ab88ad4f
Alex Gusev <alex@flancer64.com>
2015-05-21 19:01:16 +0300

    AD-310 - Add retail bonus grid to admihtml

---
+++ src/app/code/community/Praxigento/Bonus/controllers/Adminhtml/Own/Sales/Bonus/Retail.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright (c) 2015, Praxigento
+ * All rights reserved.
+ */
+
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+class Praxigento_Bonus_Adminhtml_Own_Sales_Bonus_Retail extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
